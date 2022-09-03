d6b5d64d86c6306c00989544bcebae4efda8cd92
ronygithub <ar.rony87@gmail.com>
2022-09-03 12:51:15 +0600

    Ifram Testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className='aurno-header'>
+        <h1>
+          Aurno TvList
+        </h1>
+      </div>
+      <main className='aurno-app'>
+      <iframe src="//stream.crichd.vip/update/sscricket.php" width={100} height={500}allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full" ></iframe>
+      </main>
+    </React.Fragment>
   );
 }
 
